53344d351bc919ca0c9a872bc7b6e8b911e15c31
Ayush Sagar <ayushsagar@gmail.com>
2015-03-18 12:14:38 +0530

    added file list generation messages

--- source/Main.m
+++ source/Main.m
@@ -22,18 +22,32 @@ function Main(DataSetPath, AlgoList, JointAlgo)
 %   Main('D:\testset', {'someoldguy2002','someoldguy2007','chen2011'},'chen2011')
 %
 
-%% Initialize
-% variables
+%% init variables
 CostList = [];
 AccuracyList = [];
 LabelList = [];
 ElapsedSeconds = 0;
 
-% set time format
+%% set time format
 global formatOut
 formatOut = 'HH:MM:SS:FFF dd-mmm-yy';
 
-% load ground truth file
+%% generate dataset image list
+StartTime = now;
+disp([datestr(StartTime,formatOut), ': *started dataset image list generation'])
+
+if DataSetPath(end) == '\' 
+    DataSetPath = DataSetPath(1:end-1);
+end
+Files = dir([DataSetPath, '\*.png']);
+global FileNames
+FileNames = sort_nat({Files.name});
+
+EndTime = now;
+ElapsedSeconds = (EndTime-StartTime) * 24 * 60 * 60;
+disp([datestr(EndTime,formatOut), ':  finished dataset image list generation in ', num2str(ElapsedSeconds), ' seconds'])
+
+%% load ground truth file
 StartTime = now;
 disp([datestr(StartTime,formatOut), ': *started loading ground truth file'])
 global digitStruct
@@ -42,15 +56,7 @@ EndTime = now;
 ElapsedSeconds = (EndTime-StartTime) * 24 * 60 * 60;
 disp([datestr(EndTime,formatOut), ':  finished loading ground truth file in ', num2str(ElapsedSeconds), ' seconds'])
 
-% load files list
-if DataSetPath(end) == '\' 
-    DataSetPath = DataSetPath(1:end-1);
-end
-Files = dir([DataSetPath, '\*.png']);
-global FileNames
-FileNames = sort_nat({Files.name});
-
-%% Get points for CPU protocol by iterating through list of algorithms
+%% get points for CPU protocol by iterating through list of algorithms
 for AlgoName = AlgoList
     StartTime = now;
     disp([datestr(StartTime,formatOut), ': *started processing dataset with ', AlgoName{1}, ' algorithm'])
@@ -66,7 +72,7 @@ for AlgoName = AlgoList
     disp([datestr(EndTime,formatOut), ':  finished processing dataset in ', num2str(ElapsedSeconds), ' seconds'])
 end
 
-%% Get points for CPU+HPU protocol
+%% get points for CPU+HPU protocol
 StartTime = now;
 disp([datestr(StartTime,formatOut), ': *started processing dataset with joint algorithm ', JointAlgo, '+HPU'])
 [temp1, temp2] = ProcessDataset('CPU+HPU', str2func(JointAlgo), DataSetPath);
@@ -78,7 +84,7 @@ disp([datestr(EndTime,formatOut),':  evaluated accuracy is ',num2str(temp2), '% 
 ElapsedSeconds = (EndTime-StartTime) * 24 * 60 * 60;
 disp([datestr(EndTime,formatOut), ':  finished processing dataset in ', num2str(ElapsedSeconds), ' seconds'])
 
-%% Get points for HPU protocol
+%% get points for HPU protocol
 StartTime = now;
 disp([datestr(now,formatOut), ': *started processing dataset with HPU'])
 [temp1, temp2] = ProcessDataset('HPU', '', DataSetPath);
@@ -90,21 +96,21 @@ disp([datestr(EndTime,formatOut),':  evaluated accuracy is ',num2str(temp2), '% 
 ElapsedSeconds = (EndTime-StartTime) * 24 * 60 * 60;
 disp([datestr(EndTime,formatOut), ':  finished processing dataset in ', num2str(ElapsedSeconds), ' seconds'])
 
-%% Output
+%% output
 
-% Draw plot
+% draw plot
 scatter(CostList, AccuracyList, 'fill')
 xlabel('Cost (seconds)');
 ylabel('Accuracy (%)');
 
-% Override axis
+% override axis
 xmin = 0;
 xmax = max(CostList) * 1.1; % Adds 10% margin
 ymin = 0;
 ymax = 100;
 axis([xmin, xmax, ymin, ymax])
 
-% Fill in labels
+% fill in labels
 for I = 1:size(CostList')
     text(CostList(I), AccuracyList(I), strcat({'  '}, LabelList{I}))
 end
